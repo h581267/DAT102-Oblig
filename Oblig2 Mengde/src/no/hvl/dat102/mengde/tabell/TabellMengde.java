276a04fd65d9c1eb86fe434829c41f8d5b33e409
h581267 <54098876+h581267@users.noreply.github.com>
2020-02-18 14:57:28 +0100

    18.02.2019

---
+++ Oblig2 Mengde/src/no/hvl/dat102/mengde/tabell/TabellMengde.java
@@ -0,0 +1,219 @@
+package no.hvl.dat102.mengde.tabell;
+
+import java.util.Iterator;
+import java.util.Random;
+
+import no.hvl.dat102.exception.EmptyCollectionException;
+import no.hvl.dat102.mengde.adt.*;
+
+public class TabellMengde<T> implements MengdeADT<T> {
+	// ADT-en Mengde implementert som tabell
+	//
+	private final static Random tilf = new Random();
+	private final static int STDK = 100;
+	private int antall;
+	private T[] tab;
+
+	public TabellMengde() {
+		this(STDK);
+	}
+
+	public TabellMengde(int start) {
+		antall = 0;
+		tab = (T[]) (new Object[start]);
+	}
+
+	@Override
+	public int antall() {
+		return antall;
+	}
+
+	public T[] getTab() {
+		return tab;
+	}
+
+	@Override
+	public boolean erTom() {
+		return (antall == 0);
+	}
+
+	@Override
+	public void leggTil(T element) {
+		if (!inneholder(element)) {
+			if (antall == tab.length) {
+				utvidKapasitet();
+			}
+			tab[antall] = element;
+			antall++;
+		}
+	}
+
+	private void utvidKapasitet() {
+		T[] hjelpetabell = (T[]) (new Object[2 * tab.length]);
+		for (int i = 0; i < tab.length; i++) {
+			hjelpetabell[i] = tab[i];
+		}
+		tab = hjelpetabell;
+	}
+
+	@Override
+	public T fjernTilfeldig() {
+		if (erTom())
+			throw new EmptyCollectionException("mengde");
+
+		T svar = null;
+		int indeks = tilf.nextInt(antall);
+		svar = tab[indeks];
+		tab[indeks] = tab[antall - 1];
+		antall--;
+
+		return svar;
+	}
+
+	@Override
+	public T fjern(T element) {
+		// Søker etter og fjerner element. Returnerer null-ref ved ikke-funn
+
+		if (erTom())
+			throw new EmptyCollectionException("Mengde er tom");
+
+		boolean funnet = false;
+		T svar = null;
+		int i = 0;
+
+		while (!funnet && i < antall) {
+			if (tab[i] == element) {
+				svar = tab[i];
+
+				for (int j = i; j < antall; j++) {
+					tab[j] = tab[j + 1];
+				}
+				antall--;
+
+			}
+			i++;
+		}
+
+		return svar;
+	}
+
+	@Override
+	public boolean inneholder(T element) {
+		boolean funnet = false;
+		for (int i = 0; (i < antall) && (!funnet); i++) {
+			if (tab[i].equals(element)) {
+				funnet = true;
+			}
+		}
+		return (funnet);
+	}
+
+	@Override
+	public boolean equals(Object m2) {
+		if (erTom()) {
+			throw new EmptyCollectionException("Tom tabell");
+		}
+		
+		if(m2 == this) {
+			return true;
+		}
+		
+		if(m2 == null || !(m2 instanceof TabellMengde)) { //Sjekker om m2 er en tabellmengde
+			return false;
+		}
+
+		T element = null;
+		boolean likeMengder = true;
+		TabellMengde mengde2 = (TabellMengde) m2;		//Caster objectet m2 om til en tabellmengde
+
+		if (antall() == mengde2.antall()) {
+			Iterator<T> itr = mengde2.oppramser();
+
+			while (itr.hasNext() && likeMengder) {
+				element = itr.next();
+				if (!this.inneholder(element)) {
+					likeMengder = false;
+				}
+
+			}
+
+		} else {
+			likeMengder = false;
+		}
+
+		return likeMengder;
+	}
+
+	@Override
+	public void leggTilAlle(MengdeADT<T> m2) {
+		Iterator<T> teller = m2.oppramser();
+		while (teller.hasNext())
+			leggTil(teller.next());
+	}
+
+	/*
+	 * Denne versjonen av unionen er lite effekktiv
+	 * 
+	 * @Override public MengdeADT<T> union(MengdeADT<T> m2) { TabellMengde<T> begge
+	 * = new TabellMengde<T>(); for (int i = 0; i < antall; i++) {
+	 * begge.leggTil(tab[i]); } Iterator<T> teller = m2.oppramser();
+	 * 
+	 * while (teller.hasNext()) { begge.leggTil(teller.next()); } return
+	 * (MengdeADT<T>)begge; }
+	 */
+	@Override
+
+	public MengdeADT<T> union(MengdeADT<T> m2) {
+		MengdeADT<T> begge = new TabellMengde<T>();
+		T element = null;
+		/*
+		 * Fyll ut
+		 * 
+		 */
+		return begge;
+	}//
+
+	@Override
+	public MengdeADT<T> snitt(MengdeADT<T> m2) {
+		MengdeADT<T> snittM = new TabellMengde<T>();
+		T element = null;
+		/*
+		 * Fyll ut
+		 */
+		return snittM;
+	}
+
+	@Override
+	public MengdeADT<T> differens(MengdeADT<T> m2) {
+		MengdeADT<T> differensM = new TabellMengde<T>();
+		T element;
+		/*
+		 * Fyll ut
+		 * 
+		 * if (!m2.inneholder(element)) ((TabellMengde<T>) differensM).settInn(element);
+		 */
+
+		return differensM;
+	}
+
+	@Override
+	public boolean undermengde(MengdeADT<T> m2) {
+		boolean erUnderMengde = true;
+		// Fyll ut
+		return false;
+	}
+
+	@Override
+	public Iterator<T> oppramser() {
+		return new TabellIterator<T>(tab, antall);
+	}
+
+	private void settInn(T element) {
+		if (antall == tab.length) {
+			utvidKapasitet();
+		}
+		tab[antall] = element;
+		antall++;
+	}
+
+}// class
